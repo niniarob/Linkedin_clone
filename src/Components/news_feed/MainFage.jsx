8b81971852c60d82300c68f4d3cea3e30b9c651d
niniarob <robakidzenino89@gmail.com>
2023-10-30 02:33:21 +0400

    linkedinmaipage

--- src/Components/news_feed/MainFage.jsx
+++ src/Components/news_feed/MainFage.jsx
@@ -1,7 +1,21 @@
+
 import React, { useRef } from 'react';
 import './MainFage.css';
 import Assets from './assets/Logo 1.png';
-// import Assets from './assets/Logo 1.png';
+import Crown from './assets/Crown 1.png';
+import Ellipse from './assets/Ellipse 16.png';
+import Photo from './assets/1 1.png';
+import Video from './assets/2 1.png';
+import Event from './assets/3 1.png';
+import Article from './assets/4 1.png';
+import Arrow from './assets/Angle-down.png';
+import Ellips from './assets/Ellipse 16 (1).png';
+import Times from './assets/Times.png';
+import Component from './assets/Component 3.png';
+import Vlad from './assets/Vlad.png';
+import Coment from './assets/Coment.png';
+import Avatar from './assets/Avatar.png';
+
 
 
 
@@ -22,6 +36,7 @@ export default function FileUpload() {
 
   return (
     <div className='impDiv'>
+
       <header>
         <img className='logoImage' src={Assets} alt="Logo 1" />
         <div className='headerLinks'>
@@ -43,13 +58,6 @@ export default function FileUpload() {
         </div>
         </div>
       </header>
-
-
-
-
-
-
-
 
       <main>
         <div>
@@ -75,70 +83,187 @@ export default function FileUpload() {
                 </div>
             </div>
             <div>
-              <h4>Access exclusive tools & insights</h4>
-              
+              <div className='crownDiv'>
+                <div className='access'>
+                   <h4 className='text-access'>Access exclusive tools & insights</h4>
+                <div className='photoAndText'>
+                   <img className='crownImg' src={Crown} alt="Crown 1"/>
+                   <p>Try Premium for free</p>
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
+          <div className='postPart'>
+          <div className='postSomething'>
+            <img className='profilePhoto' src={Ellipse} alt='Ellipse 16'/>
+            <input className='postInput' type='text' placeholder='Post something...' />
+          </div>
+          <div className='iconsLinks'>
+          <div className='icons'>
+             <img src={Photo} alt='1 1'/>
+             <h5>Photo</h5>
+          </div>
+          <div className='icons'>
+             <img src={Video} alt='2 1'/>
+             <h5>Video</h5>
+          </div>
+          <div className='icons'>
+             <img src={Event} alt='3 1'/>
+             <h5>Event</h5>
+          </div>
+          <div className='icons'>
+             <img src={Article} alt='4 1'/>
+             <h5>Write article</h5>
+          </div>
+          </div>
+          <div className='sortBy'>
+            <div className='secLine'></div>
+            <h5 className='sort'>Sort by:</h5>
+            <div className='top'>
+              <h4 className='sort Top'>Top</h4>
+              <img className='arrow' src={Arrow} alt='Angle-down'/>
             </div>
-        </div>
-        <div></div>
-        <div></div>
+          </div>
 
+          <div className='postCard'>
+            <div className='userNameAndPhoto'>
+              <img className="namee" src={Ellips} alt='Ellipse 16 (1)'/>
+              <div className='userNameText'>
+                <h3 className='name'>
+                  ninoi robakidze
+                </h3>
+                <p className='userPrg'>
+                CEO, Innovation, Technology 
+                </p>
+              </div>
+            <div className='closeArrow'>
+              <div className='arrowC'>
+              <img src={Component} alt='Component 3'/>
+              <img src={Times} alt='Times.png'/>
+              </div>
+              <div className='follow'>
+                Follow
+              </div>
+            </div>
+          </div>        
+          <div className='statusPart'>
+          Smart innovation in Taiwan - 
+          Gogoro technology electric scooter battery
+          swap solutions make urban sustainability possible.
+          </div>
+          <div className='imageSrc'>
+            <img className='imageSRC' src={Vlad} alt='Vlad.png'/>
+          </div>
+          <div className='comment'>
+            80 Comments
+          </div>
+          <div className='line lin'></div>
+          <div className='comments'>
+            <img src={Coment} alt='Coment.png'/>
+            <p className='cmn'>Coment</p>
+          </div>
+          </div>
+          <div className='postCard secPostCard'>
+            <div className='userNameAndPhoto'>
+              <img className="namee" src={Ellips} alt='Ellipse 16 (1)'/>
+              <div className='userNameText'>
+                <h3 className='name'>
+                  ninoi robakidze
+                </h3>
+                <p className='userPrg'>
+                CEO, Innovation, Technology 
+                </p>
+              </div>
+            <div className='closeArrow'>
+              <div className='arrowC'>
+              <img src={Component} alt='Component 3'/>
+              <img src={Times} alt='Times.png'/>
+              </div>
+              <div className='follow'>
+                Follow
+              </div>
+            </div>
+          </div>        
+          <div className='statusPart'>
+          Smart innovation in Taiwan - 
+          Gogoro technology electric scooter battery
+          swap solutions make urban sustainability possible.
+          </div>
+          <div className='imageSrc'>
+            <img className='imageSRC' src={Vlad} alt='Vlad.png'/>
+          </div>
+          <div className='comment'>
+            80 Comments
+          </div>
+          <div className='line lin'></div>
+          <div className='comments'>
+            <img src={Coment} alt='Coment.png'/>
+            <p className='cmn'>Coment</p>
+          </div>
+          </div>
+          </div>
+          <div className='addToFeedPart'>
+            <div className='feed'>
+              <h>Add to your feed</h>
+              <div className='feedLists'>
+                <div className='lists'>
+                  <img src={Avatar} alt='Avatar.png'/>
+                  <div className='secLists'>
+                    <p className='feedUsers'>ANZ OILFIELD SERVICES </p>
+                    <p className='prg'>Company, Oil & Energy</p>
+                    <div className='follow'>
+                        Follow
+                    </div>
+                  </div>
+
+                </div>
+                <div className='lists'>
+                  <img src={Avatar} alt='Avatar.png'/>
+                  <div className='secLists'>
+                    <p className='feedUsers'>ANZ OILFIELD SERVICES </p>
+                    <p className='prg'>Company, Oil & Energy</p>
+                    <div className='follow'>
+                        Follow
+                    </div>
+                  </div>
+
+                </div>
+                <div className='lists'>
+                  <img src={Avatar} alt='Avatar.png'/>
+                  <div className='secLists'>
+                    <p className='feedUsers'>ANZ OILFIELD SERVICES </p>
+                    <p className='prg'>Company, Oil & Energy</p>
+                    <div className='follow'>
+                        Follow
+                    </div>
+                  </div>
+
+                </div>
+                <div className='lists'>
+                  <img src={Avatar} alt='Avatar.png'/>
+                  <div className='secLists'>
+                    <p className='feedUsers'>ANZ OILFIELD SERVICES </p>
+                    <p className='prg'>Company, Oil & Energy</p>
+                    <div className='follow'>
+                        Follow
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+              <div>
+              </div>
+              </div>
+                <p className='recomendation'>
+                     View all recommendations 
+                </p>
+          </div>
       </main>
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-
-// export default function FileUpload() {
-//   const [selectedFile, setSelectedFile] = useState(null);
-
-//   const handleFileChange = (e) => {
-//     const file = e.target.files[0];
-//     setSelectedFile(file);
-//   };
-
-//   const handleFileUpload = () => {
-//     if (selectedFile) {
-//       const formData = new FormData();
-//       formData.append('file', selectedFile);
-
-//       // Replace 'your-upload-endpoint' with the actual URL where you want to upload the file.
-//       fetch('your-upload-endpoint', {
-//         method: 'POST',
-//         body: formData,
-//       })
-//         .then((response) => {
-//           if (response.ok) {
-//             // File uploaded successfully
-//             console.log('File uploaded successfully');
-//           } else {
-//             // Handle the error
-//             console.error('File upload failed');
-//           }
-//         })
-//         .catch((error) => {
-//           console.error('Error:', error);
-//         });
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="file" onChange={handleFileChange} />
-//       <button onClick={handleFileUpload}>Upload</button>
-//     </div>
-//   );
-// }
+      <footer>
+        
+      </footer>
+ </div>
+  )
+};
