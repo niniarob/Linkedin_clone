7b832d0753e150deb2644f8ae31a1fee5f693db7
niniarob <robakidzenino89@gmail.com>
2023-11-03 00:58:15 +0400

    profile page is almost ready, small things are still not finished

--- src/Components/Main_profile_page/Header.jsx
+++ src/Components/Main_profile_page/Header.jsx
@@ -1,11 +1,34 @@
 import './Header.css'
+import Logo from './assets/Logo 1 (1).png'
+import EllipseProfile from './assets/Ellipse 14.svg'
+import Search from "./assets/search.svg"
 
 
 
 export default function Header(){
     return(
-        <div>
-            header
+        <>        
+        <div className='impDiv'>
+            <header>
+                <div className='logoImput'>
+                    <img src={Logo} alt='./logo 1 (1).png'/>
+                    <input className='searchInputInProfile' type = "search"
+                    placeholder = "Search"
+                    />
+                </div>
+                <div className='listsImg'>
+                <ul>
+                    <li>Home</li>
+                    <li>My Network</li>
+                    <li>Jobs</li>
+                    <li>Messaging</li>
+                    <li>Notifications</li>
+                </ul>
+                <img src={EllipseProfile} alt='./Ellipse 14.svg'/>
+                </div>
+            </header>
         </div>
+        </>
+
     )
 }
